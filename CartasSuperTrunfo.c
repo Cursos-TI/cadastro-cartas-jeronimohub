150d0fab6520656fa2a53f52998e168b4c2aecaf
jeronimohub <jeronimosantosmelo13@gmail.com>
2026-04-01 17:13:23 +0000

    inserir mais duas váiaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,36 +8,59 @@ int main(){
   // Área para definição das variáveis para armazenar as propriedades das cidades
     char Estado1 = 'A';
     char Codigo1[] = "A01";
-    char Nomedacidade1[] = "São Paulo";
+    char Nomedacidade1[] = "Rio de Janeiro";
     int Populacao1 = 12325000;
-    float Area1 = 11521.11;
+    float Area1 = 1521.11;
     float Pib1 = 699.28;
     int Numerodepontosturisticos1 = 50;
+    float divisao;
+    divisao = Populacao1 / Area1;
+    float cdpib;
+    cdpib =  Pib1 / Populacao1;
+
+
+    
 
     char Estado2 = 'B';
     char Codigo2[] = "B02";
     char Nomedacidade2[] = "São Paulo";
-    int Populacao2 = 12325000;
-    float Area2 = 11521.11;
-    float Pib2 = 699.28;
+    int Populacao2 = 6748000;
+    float Area2 = 1200.25;
+    float Pib2 = 300.50;
     int Numerodepontosturisticos2 = 50;
+    float divisao2;
+    divisao2 = Populacao2 / Area2;
+    float cdpib1;
+    cdpib1 =  Pib2 / Populacao2;
+    
+
 
   // Área para entrada de dados
-    printf("Estado1: %c \n", Estado1);
-    printf("Codigo1: %s \n", Codigo1);
-    printf("Nomedacidade1: %s \n", Nomedacidade1);
-    printf("Populacao1: %d \n", Populacao1);
-    printf("Area1: %f Km² \n", Area1);
-    printf("Pib1: %f bilhões de reais \n", Pib1);
-    printf("Numerodepontosturisticos1: %d \n \n", Numerodepontosturisticos1 );
+    printf("Carta1\n");
+    printf("Estado: %c \n", Estado1);
+    printf("Código: %s \n", Codigo1);
+    printf("Nome da Cidade: %s \n", Nomedacidade1);
+    printf("População: %d \n", Populacao1);
+    printf("Área: %.2f Km² \n", Area1);
+    printf("PIB: %.2f bilhões de reais \n", Pib1);
+    printf("Número de Pontos Turísticos1: %d \n", Numerodepontosturisticos1 );
+    printf("Densidade Populacional: %.2f hab/km² \n ", divisao);
+    printf("PIB per Capita: %f reais \n \n", cdpib);
 
-    printf("Estado2: %c \n", Estado2);
-    printf("Codigo2: %s \n", Codigo2);
-    printf("Nomedacidade2: %s \n", Nomedacidade2);
-    printf("Populacao2: %d \n", Populacao2);
-    printf("Area2: %f Km² \n", Area2);
-    printf("Pib2: %f bilhões de reais \n", Pib2);
-    printf("Numerodepontosturisticos2: %d \n", Numerodepontosturisticos2);
+
+
+    printf("Carta2\n");
+    printf("Estado: %c \n", Estado2);
+    printf("Código2: %s \n", Codigo2);
+    printf("Nome da Cidade2: %s \n", Nomedacidade2);
+    printf("População: %d \n", Populacao2);
+    printf("Área2: %.2f Km² \n", Area2);
+    printf("PIB: %.2f bilhões de reais \n", Pib2);
+    printf("Número de Pontos Turísticos: %d \n", Numerodepontosturisticos2);
+    printf("Densidade Populacional: %.2f hab/km² \n", divisao2);
+    printf("PIB per Capita: %f reais \n \n", cdpib1);
+
+
 
   //Área para exibição dos dados da cidade
 
---
+++ ee.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("***Programa de Cálculo de Média***\n");
+
+    printf("Digite sua primeira nota: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite sua segunda nota: \n");
+    scanf("%f", &nota2);
+
+    printf("Digite sua terceira nota:\n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) /3;
+      /*conversão implicita media = (float)(nota1 + nota2 + nota3) /3;*/
+      
+    printf("A sua média é: %.1f\n", media);
+
+    return 0;
+
+
+
+}
